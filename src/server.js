ae96fa3adb832d45b07f25c84a38787ebd00a759
Ryan Wire Simiyu <raywire@Ryans-MacBook-Pro.local>
2019-05-24 13:55:04 +0300

    [#166234238] Updates heroku configuration

--- src/server.js
+++ src/server.js
@@ -8,9 +8,9 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // Static files
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist/')));
 
 // wildcard route
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + 'index.html'));
 });
