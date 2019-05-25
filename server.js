85b2b4dd33fe67a86ca7ed4798c20c61f2cc7d35
Ryan Wire Simiyu <raywire@Ryans-MacBook-Pro.local>
2019-05-25 09:01:26 +0300

    [#166234238] Updates server,js file

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // Static files
-app.use(express.static(path.join(__dirname, 'dist/')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 // wildcard route
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/src/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
